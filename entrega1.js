a077626c384b99032f156c06bf8857849ff600af
Jopo91 <jos.rodb16@gmail.com>
2022-09-07 22:50:42 -0500

    actualizacion

--- entrega1.js
+++ entrega1.js
@@ -1,9 +1,8 @@
-console.log("Buenas tardes")
 let qty = parseInt(promt("Cuantos numeros deseas sumar"));
-
 
 for (let i = 1; i <= qty; i++){
     let nums = parseInt(promt("Ingresa un numero"))
+    while(nums != 0);
         let suma = suma+nums
 }
 
